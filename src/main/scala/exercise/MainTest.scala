99457f63a814a22a67c884600c28f93d6516a526
roy2015 <roy2010@163.com>
2017-08-23 15:30:39 +0800

    写几个scala的test

---
+++ src/main/scala/exercise/MainTest.scala
@@ -0,0 +1,45 @@
+package exercise
+
+/**
+  * Created by BG244210 on 23/08/2017.
+  */
+object MainTest {
+  /**
+    * 测试数组
+    */
+  def test1 (): Unit = {
+    val arry = Array(1,2,3)
+    for (i <- arry) {
+      println(i)
+    }
+  }
+
+  /**
+    * wordCount
+    */
+  def test2(): Unit = {
+    //wordcount 1
+    val list = List("guo", "jun", "roy" ,"guo", "jun", "jun")
+    println( list.map( (_, 1)) )
+    println( list.map( (_, 1)).groupBy(_._1) )
+    println( list.map( (_, 1)).groupBy(_._1).mapValues( x => x.foldLeft(0)((a,b) => a + b._2) ) )
+    val map = list.map( (_, 1)).groupBy(_._1).mapValues( x => x.foldLeft(0)((a,b) => a + b._2) )
+    val listfin = map.toList
+    println(listfin)
+    println( listfin.sortBy(_._2) )
+    println()
+
+    //wordcount 2
+    val list1 = List("guo roy jun", "roy guo1 jun1", "guo guo guo1 guo1 jun")
+    println( list1.flatMap( _.split(" ")).map( (_, 1)).groupBy(_._1).mapValues( x => x.foldLeft(0)((a,b) => a + b._2)))
+    println( list1.flatMap( _.split(" ")).map( (_, 1)).groupBy(_._1).mapValues( x => x.foldLeft(0)((a,b) => a + b._2)).toList.sortBy(_._2))
+  }
+
+
+
+
+  def main(args: Array[String]) {
+//    test1();
+    test2()
+  }
+}
