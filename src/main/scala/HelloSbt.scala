8652e2083ae1a0bab1674f4948f6f63e17c5ae3c
roy2015 <roy2010@163.com>
2017-08-23 10:25:18 +0800

    scala引用第三方jar包（比如自己的jar）

--- src/main/scala/HelloSbt.scala
+++ src/main/scala/HelloSbt.scala
@@ -1,4 +1,6 @@
+import com.best.v8.common.biz.util.DateUtil
 import org.apache.commons.lang3.StringUtils
+import java.util.Date
 
 /**
   * Created by BG244210 on 22/08/2017.
@@ -9,5 +11,7 @@ object HelloSbt {
 
     val s = StringUtils.leftPad("90", 5,'0')
     println(s)
+
+    println( DateUtil.dateFormatFull.format( DateUtil.getYearsAfter(new Date(), 1)))
   }
 }
